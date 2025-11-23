dd919c4a193b78c8043ad9af4ec5f96de0a07e0d
hishmatrai <hishmatrai.2018@gmail.com>
2025-11-23 18:03:51 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import React from "react";
-import WebImg from "./web.png";
 import "./App.css";
 import Home from "./Home";
 import About from "./About";
+import Contact from "./Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div>
-      <Home />
-      {/* <About /> */}
-      {/* <h1>Web Development</h1>
-      <h1 id="heading">Web Development</h1>
-      <h1 className="web-dev">Web Development</h1>
-      <h1>Web Development</h1>
-      <h1>Web Development</h1>
-      <h1 className="web-dev">Web Development</h1>
-      <p>
-        Web development is the process of creating and maintaining websites and
-        web applications, encompassing front-end (what the user sees and
-        interacts with) and back-end (server, database, and application logic)
-        development. It involves a range of tasks, from basic design and coding
-        to complex web applications, and requires skills in programming
-        languages like HTML, CSS, and JavaScript, as well as database management
-        and other technical fields.
-      </p>
-      <ul>
-        <li>web development</li>
-        <li>app development</li>
-      </ul>
-      <input placeholder="Type ..." />
-      <hr />
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvGCDR5nBpsRnjkCoIccrnqoKpDSYUTG7MLA&s" />
-      <hr />
-      <img src={WebImg} />
-      <hr />
-      <hr />
-      <img src={require("./web.png")} /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/contact-us" element={<Contact />}/>
+        <Route path="*" element={<h1>Page not Found!</h1>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,10 @@
 import React, { useState } from "react";
 const Home = () => {
-  const [color, setColor] = React.useState("blue");
-  const [username, setUsername] = useState("Web Development");
-  const updateUserName = () => {
-    setUsername("FDKFJ");
-  };
-
-  const colors = ["Red", "Green", "Blue", "Yellow", "Black"];
   return (
     <div>
-      <h1 id="home-heading" style={{ backgroundColor: color }}>
+      <h1 >
         Home Page
       </h1>
-      <button onClick={() => setColor("red")}>Change Color</button>
-      <p>{username}</p>
-      <button onClick={() => setUsername("ABC")}>Change Name</button>
-      <button onClick={updateUserName}>Update</button>
-      <button onClick={() => updateUserName()}>Update</button>
-
-      <ul>
-        {colors.map((val, index) => {
-          return <li>{val}</li>;
-        })}
-      </ul>
-
-      <input type="text" placeholder="Type ..." value="fsdfds"/>
     </div>
   );
 };
