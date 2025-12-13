c148712de98dc9986035ace033efe777609997a9
hishmatrai <hishmatrai.2018@gmail.com>
2025-12-13 19:00:00 +0500

    update

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,15 +1,24 @@
 import React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 const Footer = () => {
   return (
-    <div style={{
-        backgroundColor:"blue",
-        display:"flex",
-        alignItems:"center",
-        padding:"5px",
-        justifyContent:"center",
-        color:"white"
-    }}>
-      <p style={{fontStyle:"italic"}}>Footer</p>
+    <div
+      style={{
+        // backgroundColor: "blue",
+        display: "flex",
+        alignItems: "center",
+        padding: "5px",
+        justifyContent: "center",
+        color: "white",
+      }}
+    >
+      <p style={{ fontStyle: "italic" }}>Footer</p>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button color="secondary">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
     </div>
   );
 };
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { Layout } from "../../components";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import { BiLogoFacebook } from "react-icons/bi";
+
 const Contact = () => {
   return (
     <Layout activePage="Contact" hideFooter={true}>
       <h1>Contact Data</h1>
+      <FacebookIcon style={{ color: "green", width: "50px", height: "50px" }} />
+      <ArrowForwardIcon />
+      <AccessTimeFilledIcon style={{ color: "blue", width: "150px", height: "150px" }} />
+      <BiLogoFacebook />
     </Layout>
   );
 };
